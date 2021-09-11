4636365e069a11480cd647233c67dae1be848279
Aaron Tran <altran32@gmail.com>
2021-09-11 14:22:45 -0700

    reformat per eslint req

--- client/src/components/QA/Form.jsx
+++ client/src/components/QA/Form.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Form = (props) => {
-  return (
-    <div>
-      Questions & Answers
-      {/* {props} */}
-      <form placeholder="Ask Your Question Here"> </form>
-    </div>
-  );
-};
+const Form = (props) => (
+  <div>
+    Questions & Answers
+    {/* {props} */}
+    <form placeholder="Ask Your Question Here"> </form>
+  </div>
+);
 
 export default Form;
