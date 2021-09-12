db6e96d55195947935b8d902391cbb3cb36b1ab2
Aaron Tran <altran32@gmail.com>
2021-09-12 15:09:01 -0700

    also add eslint plugin react hooks linter to ensure consistency of hook usage

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -14,7 +14,7 @@ const Modal = () => {
         <div>
           <div>
             <h2>
-              Hello Modal
+              Hello Modal!
             </h2>
             <p>
               CONTENT!
