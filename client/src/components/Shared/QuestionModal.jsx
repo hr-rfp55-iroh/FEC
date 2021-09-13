bfaa4fa672074d316f5f4eb4ed903a64f4c4c912
Aaron Tran <altran32@gmail.com>
2021-09-13 10:53:56 -0700

    css and form adjustments. need to address positioning of close modal/ submit button

--- client/src/components/Shared/QuestionModal.jsx
+++ client/src/components/Shared/QuestionModal.jsx
@@ -3,31 +3,34 @@ import React, { useState } from 'react';
 const QuestionModal = (props) => {
   const [modal, setModal] = useState(false);
   const [text, setText] = useState('');
+  const [email, setEmail] = useState('');
   const toggleModal = () => {
     setModal(!modal);
   };
-
 
   return (
     <div>
       <button className="button-modal" onClick={toggleModal} type="submit">Submit A Question</button>
       {modal && (
         <div className="modal">
-          {/* <div className="overlay" role="button" tabIndex="0"> */}
-          <div onClick={toggleModal} onKeyPress={toggleModal}
-            className="overlay" role="button" tabIndex="0">
+          <div className="overlay" role="button" tabIndex="0">
+            {/* <div onClick={toggleModal} onKeyPress={toggleModal} */}
+            {/* className="overlay" role="button" tabIndex="0"> */}
             <div className="modal-content">
 
               <h2>
                 Your Question:
               </h2>
-              <textarea maxLength="1000" value={text} onChange={(e) => setText(e.target.value)} />
-              <p>
-                Content
-              </p>
+              <div>What is your question? (required)</div>
+              <br />
+              <textarea maxLength="1000" value={text} onChange={(e) => setText(e.target.value)} placeholder="Why did you like the product or not?" />
+              <br />
+              <div>What is your email? (required)</div>
+              <br />
+              <textarea maxLength="60" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="jackson11!" />
             </div>
 
-            <button className="submit-question" type="submit">Submit Form</button>
+            <button className="submit-button" type="submit">Submit Form</button>
             <button className="close-modal" type="submit" onClick={toggleModal}>Close The Modal </button>
           </div>
         </div>
