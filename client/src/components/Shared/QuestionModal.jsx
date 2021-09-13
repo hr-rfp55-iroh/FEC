a5d1e073f1f41ef74bd3e9aa72c7e09bc53f5792
Aaron Tran <altran32@gmail.com>
2021-09-13 10:27:36 -0700

    able to capture text from textfield with useState hook

--- client/src/components/Shared/QuestionModal.jsx
+++ client/src/components/Shared/QuestionModal.jsx
@@ -2,25 +2,32 @@ import React, { useState } from 'react';
 
 const QuestionModal = (props) => {
   const [modal, setModal] = useState(false);
+  const [text, setText] = useState('');
   const toggleModal = () => {
     setModal(!modal);
   };
+
 
   return (
     <div>
       <button className="button-modal" onClick={toggleModal} type="submit">Submit A Question</button>
       {modal && (
         <div className="modal">
-          <div onClick={toggleModal} onKeyPress={toggleModal} className="overlay" role="button" tabIndex="0">
+          <div className="overlay" role="button" tabIndex="0">
+            {/* <div onClick={toggleModal} onKeyPress={toggleModal}
+            className="overlay" role="button" tabIndex="0"> */}
             <div className="modal-content">
 
               <h2>
-                Content Title
+                Your Question:
               </h2>
+              <textarea maxLength="1000" value={text} onChange={(e) => setText(e.target.value)} />
               <p>
                 Content
               </p>
             </div>
+
+            <button className="submit-question" type="submit">Submit Form</button>
             <button className="close-modal" type="submit" onClick={toggleModal}>Close The Modal </button>
           </div>
         </div>
