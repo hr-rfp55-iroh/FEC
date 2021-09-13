2784a10c05c78024b960a27e70a9833dd5048fa1
Aaron Tran <altran32@gmail.com>
2021-09-13 13:45:28 -0700

    refactored for readability

--- client/src/components/Shared/QuestionModal.jsx
+++ client/src/components/Shared/QuestionModal.jsx
@@ -9,31 +9,32 @@ const QuestionModal = (props) => {
   const toggleModal = () => {
     setModal(!modal);
   };
-  const handleQuestionSubmit = (e) => {
-    // TODO : insert post route here that takes in obj
+  const handleValidationAndSubmit = (e) => {
     e.preventDefault();
+    const missingFields = {};
     let isFieldsFilled = true;
     const obj = { question: text, email, nickname };
     console.log(obj);
     if (text.length === 0) {
-      setErrors({ question: 'fill out question' })
+      isFieldsFilled = false;
+      // setErrors({ question: 'fill out question' })
+      missingFields.question = 'fill out question';
     }
     if (email.length === 0) {
-      setErrors({ email: 'fill out email' })
+      isFieldsFilled = false;
+      missingFields.email = 'fill out email';
     }
     if (nickname.length === 0) {
-      setErrors({ nickname: 'fill out nickname' })
+      isFieldsFilled = false;
+      missingFields.nickname = 'fill out nickname';
     }
+    setErrors({ missingFields });
 
-
-    // let requiredFields = [`${text === '' ? 'Question' : ''}, ${email === '' ? 'Email' : ''}, ${nickname === '' ? 'Nickname' : ''}`];
-    // const isFilledIn = text !== '' || email !== '' || nickname !== '';
-    // if (isFilledIn) {
-    //   console.log('All Fields Provided!');
-    // } else {
-    //   alert('Please fill in required fields: ' + requiredFields);
-    // }
-    // if (name.length === 0) {
+    // let requiredFields = [`${text === '' ? 'Question' : ''},
+    // ${email === '' ? 'Email' : ''}, ${nickname === '' ? 'Nickname' : ''}`];
+    if (isFieldsFilled) {
+      // TODO : insert post route here that takes in obj
+    }
   };
 
   return (
@@ -65,7 +66,7 @@ const QuestionModal = (props) => {
               </form>
             </div>
 
-            <button className="submit-button" type="submit" onClick={handleQuestionSubmit}>Submit Form</button>
+            <button className="submit-button" type="submit" onClick={handleValidationAndSubmit}>Submit Form</button>
             <button className="close-modal" type="submit" onClick={toggleModal}>Close The Modal </button>
           </div>
         </div>
