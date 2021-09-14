0dcdfaa51e4b5c810093b90432018dac54f4f613
Aaron Tran <altran32@gmail.com>
2021-09-13 23:35:16 -0700

    add placeholder for photos display in answers

--- client/src/components/QA/Answer.jsx
+++ client/src/components/QA/Answer.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Answer = ({ answer }) => {
   const {
-    answer_name, body, date, helpfulness, id, photos,
+    answerer_name, body, date, helpfulness, id, photos,
   } = answer;
   return (
     <div>
       <a><strong>A:</strong></a>
-      {body}
+      {body}<br />
+      by {answerer_name}, {date} | Helpful? Yes({helpfulness}) | Report
+      <div>
+        {photos.length > 0 ? 'there are photos, map me' : ''}
+      </div>
     </div>
   );
 };
