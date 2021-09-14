7424a6b99bdc0ae03454a762e5fa71881a3d914e
Jandean Deocampo <jandeo92@gmail.com>
2021-09-13 17:46:53 -0700

    Handled conditional selection for product styles.

--- client/src/components/PO/Style.jsx
+++ client/src/components/PO/Style.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 const Style = (props) => {
+  const {
+    selected,
+    thumb,
+    name,
+    selectedStyle,
+    setSelectedStyle,
+  } = props;
+
+  const handleSelection = () => {
+    setSelectedStyle(selectedStyle);
+  };
+
   return (
-    <div></div>
+    <div
+      role="button"
+      tabIndex={0}
+      onClick={handleSelection}
+      onKeyPress={handleSelection}
+      className="style-thumb"
+    >
+      <img alt="style option" title={name} src={thumb} />
+    </div>
   );
+};
+
+Style.propTypes = {
+  selected: PropTypes.bool,
+  thumb: PropTypes.string,
+  name: PropTypes.string,
+  selectedStyle: PropTypes.number,
+  setSelectedStyle: PropTypes.func,
+};
+
+Style.defaultProps = {
+  selected: false,
+  thumb: '',
+  name: '',
+  selectedStyle: -1,
+  setSelectedStyle: null,
 };
 
 export default Style;
--- client/src/components/PO/StyleSelector.jsx
+++ client/src/components/PO/StyleSelector.jsx
@@ -1,30 +1,54 @@
+/* eslint-disable no-restricted-syntax */
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import Style from './Style';
 
 const StyleSelector = (props) => {
   const { productId } = props;
-  const [styleId, setStyleId] = useState(0);
+  const [selectedStyle, setSelectedStyle] = useState(-1);
   const [styles, setStyles] = useState([]);
 
   useEffect(() => {
     axios.get(`/po/styles/${productId}`)
       .then((results) => {
-        console.log(results.data.results);
         setStyles(results.data.results);
       });
-  }, [styleId]);
+  }, [productId]);
+
+  useEffect(() => {
+    for (const style of styles) {
+      style['default?'] = style.style_id === selectedStyle;
+    }
+  }, [selectedStyle]);
 
   const mappedList = styles.map(
-    (style) => <li>{style.name}</li>,
+    (style) => (
+      <Style
+        key={style.style_id}
+        setSelectedStyle={setSelectedStyle}
+        selectedStyle={style.style_id}
+        name={style.name}
+        selected={style['default?']}
+        thumb={style.photos[0].thumbnail_url}
+      />
+    ),
   );
 
   return (
-    <div>
+    <div id="style-selector">
       {mappedList}
     </div>
   );
+};
+
+StyleSelector.propTypes = {
+  productId: PropTypes.number,
+};
+
+StyleSelector.defaultProps = {
+  productId: 1,
 };
 
 export default StyleSelector;
