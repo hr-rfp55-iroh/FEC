7b457367ac2c4db07842eae649919e452c99418c
Aaron Tran <altran32@gmail.com>
2021-09-15 18:19:36 -0700

    search function implemented

--- client/src/components/QA/Unit.jsx
+++ client/src/components/QA/Unit.jsx
@@ -9,16 +9,14 @@ class Unit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isSearchInProgress: false,
       count: 4, // on page load, display four questions
       display: 'loadMore', // display default loadMore button
     };
     this.getQuestions = this.getQuestions.bind(this);
-    this.handleDisplayUnitOnSearch = this.handleDisplayUnitOnSearch.bind(this);
     this.getResultFromSearch = this.getResultFromSearch.bind(this);
     this.handleDisplayMoreQ = this.handleDisplayMoreQ.bind(this);
     this.handleCollapse = this.handleCollapse.bind(this);
-    this.handleDisplayButtonsOnSearch = this.handleDisplayButtonsOnSearch.bind(this);
+    this.handleDisplayUnitOnSearch = this.handleDisplayUnitOnSearch.bind(this);
   }
 
   componentDidMount() {
@@ -26,41 +24,22 @@ class Unit extends React.Component {
   }
 
   handleDisplayUnitOnSearch(e) {
-    const { searchLength, isSearchInProgress } = this.state;
+    const { searchResult, questionsList } = this.state;
     if (e.length >= 3) {
-      this.setState({ isSearchInProgress: true, display: 'search' });
+      this.setState({ display: 'none', currentList: searchResult });
     } else {
-      this.setState({ isSearchInProgress: false, display: 'loadMore' });
+      this.setState({ display: 'loadMore', currentList: questionsList });
     }
-    this.handleDisplayButtonsOnSearch()
-    // if (isSearchInProgress) {
-    //   if (searchLength < 4) {
-    //     this.setState({ display: 'none' });
-    //   } else {
-    //     this.setState({ display: 'loadMore' });
-    //   }
-    // }
   }
 
   handleDisplayMoreQ(e) {
     e.preventDefault();
-    const { count, questionsList } = this.state;
-    // console.log('count:', count, 'searchLength', searchLength);
-    console.log('count:', count, 'questionLength', questionsList.length);
+    const { count, currentList } = this.state;
     this.setState({ count: count + 2 });
-    if (count + 1 >= questionsList.length) {
+    if (count + 1 >= currentList.length) {
       this.setState({ display: 'collapse' });
     }
     // handle edge case of no question length: display only add quesiton button
-  }
-
-  handleDisplayButtonsOnSearch() {
-    const { isSearchInProgress, searchLength } = this.state;
-    if (isSearchInProgress && searchLength > 4) {
-      this.setState({ display: "loadMore" })
-      // }
-      // if(!isSearchInProgress && )
-    }
   }
 
   handleCollapse() {
@@ -73,36 +52,23 @@ class Unit extends React.Component {
       .then((results) => this.setState({
         isQuestionsLoaded: true,
         questionsList: results.data.results,
+        currentList: results.data.results,
       }))
       .catch(() => this.setState({ isQuestionsLoaded: false }));
   }
 
   getResultFromSearch(resultArr) {
-    this.setState({ searchResult: resultArr, searchLength: resultArr.length });
+    this.setState({ searchResult: resultArr });
   }
 
   render() {
     const {
-      isQuestionsLoaded, questionsList, isSearchInProgress, searchResult, count, display,
+      isQuestionsLoaded, questionsList, count, display, currentList,
     } = this.state;
     let list;
     const { currentProduct } = this.props;
-    if (isQuestionsLoaded && !isSearchInProgress) {
-      list = questionsList.map((q) => (
-        <Question
-          className="question-component"
-          key={q.question_id}
-          question_id={q.question_id}
-          question_body={q.question_body}
-          question_date={q.question_date}
-          asker_name={q.asker_name}
-          question_helpfulness={q.question_helpfulness}
-          answers={q.answers}
-        />
-      )).slice(0, count);
-      console.log('question length', list.length);
-    } else if (isSearchInProgress) {
-      list = searchResult.map((q) => (
+    if (isQuestionsLoaded) {
+      list = currentList.map((q) => (
         <Question
           className="question-component"
           key={q.question_id}
@@ -127,26 +93,18 @@ class Unit extends React.Component {
           <br />
           <QuestionModal currentProduct={currentProduct} />
           <div>
-            {/* {display === 'loadMore' ? (<button type="submit" onClick={(e) => this.handleDisplayMoreQ(e)}>Load More Questions</button>) : <button type="submit" onClick={this.handleCollapse}>collapse</button>} */}
+            {/* {display === 'loadMore' ?
+            (<button type="submit" onClick={(e) => this.handleDisplayMoreQ(e)}>
+            Load More Questions</button>)
+             :
+            <button type="submit" onClick={this.handleCollapse}>collapse</button>} */}
             {(() => {
-              console.log('hello from over here')
               if (display === 'loadMore') {
-                console.log('hello from over here 1')
-                return (
-                  <div>
-                    <button
-                      type="submit"
-                      onClick={(e) => this.handleDisplayMoreQ(e)}
-                    >
-                      Load More Questions
-                    </button>
-                  </div>
-                );
+                return (<button type="submit" onClick={(e) => this.handleDisplayMoreQ(e)}>Load More Questions</button>);
               }
               if (display === 'collapse') {
                 return (<button type="submit" onClick={this.handleCollapse}>collapse</button>);
               } if (display === 'none') {
-                console.log('hello from over here 3')
                 return ('');
               }
             })()}
