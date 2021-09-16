71b719e856eb5d537ec6efe5b5f333d8ae34f3b8
huongtran1993 <huongtran1993@gmail.com>
2021-09-15 17:50:00 -0700

    setup skeleton for create review form

---
+++ client/src/components/RR/Review/OverallRatingQuest.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OverallRatingQuest = () => {
+  const placeholder = 'Hello from OverallRatingQuest';
+  return (
+    <div>{placeholder}</div>
+  );
+};
+
+export default OverallRatingQuest;
+
+{/* <input type="text" name="overall-rating" id="overall-rating" required /> */}
---
+++ client/src/components/RR/Review/PhotoQuest.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PhotoQuest = () => {
+  const placeholder = 'Hello from PhotoQuest';
+  return (
+    <div>{placeholder}</div>
+  );
+};
+
+export default PhotoQuest;
