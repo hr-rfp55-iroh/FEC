eda8b400c71a6629184209e97d2cfa35b27ef05b
Aaron Tran <altran32@gmail.com>
2021-09-23 00:52:27 -0700

    modal works but needs tweaking

--- client/src/components/QA/Answer.jsx
+++ client/src/components/QA/Answer.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import Moment from 'moment';
+import Pic from './Pic';
 
 const Answer = ({ answer, getQuestions }) => {
   const {
@@ -10,13 +11,13 @@ const Answer = ({ answer, getQuestions }) => {
   const [reported, setReported] = useState(false);
   const [isLimitHelpful, setIsLimitHelpful] = useState(false);
   const [helpfulTrigger, setHelpfulTrigger] = useState(helpfulness);
+  const [photoModal, setPhotoModal] = useState(false);
+  const [photoSRC, setPhotoSRC] = useState('');
   const ansObj = { answer_id: id };
   const photoAlias = photos;
   const handleReportAnswer = (e) => {
     e.preventDefault();
     axios.put('/qa/answers/report', ansObj);
-    // .then(() => )
-    // .catch((error) => console.log(error));
   };
   const handleHelpfulAnswer = (e) => {
     e.preventDefault();
@@ -36,11 +37,23 @@ const Answer = ({ answer, getQuestions }) => {
       <strong>Yes</strong>
     </span>
   );
+  const togglePhotoModal = () => {
+    setPhotoModal(!photoModal);
+  };
+  // var photo;
+  const handlePhotoSrc = (e) => {
+    togglePhotoModal();
+    togglePhotoModal();
+    setPhotoSRC(e);
+  }
+  const mappedPhotos2 = photoAlias.map((photo, idx) => (<div key={photo.toString()}><img alt="" src={photo} id="answer-images" onClick={togglePhotoModal} /></div>));
+
   return (
     <div>
       <span><strong>A:</strong></span>
       {body}
       <br />
+      {photoModal && <Pic photoSRC={photoSRC} />}
       by
       {' '}
       {answerer_name.toLowerCase() === 'seller' ? <strong>{answerer_name}</strong> : answerer_name}
@@ -75,7 +88,8 @@ const Answer = ({ answer, getQuestions }) => {
       {' '}
       {/* //TODO find a way to resize image to improve efficiency */}
       <div>
-        {photoAlias.map((photo) => (<div key={photo.toString()}><img alt="" src={photo} id="answer-images" /></div>))}
+        {photoAlias.map((photo) => (<div key={photo.toString()}><img alt="" value={photo} src={photo} id="answer-images" onClick={(e) => handlePhotoSrc(e.target.src)} /></div>))}
+
       </div>
     </div>
   );
