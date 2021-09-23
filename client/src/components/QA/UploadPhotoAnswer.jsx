cc01db7ed5e0791d3ccb04063c9d245df8d00d1a
Aaron Tran <altran32@gmail.com>
2021-09-23 15:09:27 -0700

    able to display small thumbnails on photo upload

--- client/src/components/QA/UploadPhotoAnswer.jsx
+++ client/src/components/QA/UploadPhotoAnswer.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
 const UploadPhotoAnswer = (props) => {
-  const [image, setImage] = useState({ preview: [], raw: [] });
+  const [image, setImage] = useState([]);
   const [imageCount, setImageCount] = useState(0);
   const [isAnyUploaded, setIsAnyUploaded] = useState(false);
   const handleFileUpload = (e) => {
-    console.log(e)
+    console.log(e);
     setImageCount(imageCount + e.target.files.length);
     if (e.target.files.length) {
-      setImage(image.preview.concat(e.target.files));
+      // console.log('e.target.files:', e.target.files);
+      // console.log('Object.values(e.target.files)', Object.values(e.target.files));
+      const newFiles = image.slice().concat(Object.values(e.target.files));
+      setImage(newFiles);
+      // console.log('image', image);
     }
     // if (e.target.files.length) {
     //   setImage({
@@ -17,27 +21,28 @@ const UploadPhotoAnswer = (props) => {
     //   });
   };
   const handleCancelUpload = () => {
-    //set image count to 0
+    // set image count to 0
     // set image.preview to []
   };
 
   return (
     <div>
       <div>
-        Upload Your photos <br />
+        Upload Your photos
+        {' '}
+        <br />
         <input type="file" accept=".png, .jpg, .jpeg" multiple onChange={(e) => handleFileUpload(e)} />
-        <div>
+        <div className="photo">
 
-          {isAnyUploaded && image.map((img) => {
-            return (
-              // console.log(URL.createObjectURL(img));
-              < img src={URL.createObjectURL(img)} height="100" />
-            )
-          })}
+          {image.map((img) => (
+            // console.log(URL.createObjectURL(img));
+            <img src={URL.createObjectURL(img)} height="100" />
+          ))}
           {/* {console.log(URL.createObjectURL(files))} */}
         </div>
       </div>
-    </div >);
-}
+    </div>
+  );
+};
 
 export default UploadPhotoAnswer;
