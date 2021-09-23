c97a9cc18a986f5e71358752c57820adccfb25ae
Aaron Tran <altran32@gmail.com>
2021-09-23 16:49:25 -0700

    no modal on answer photo click

--- client/src/components/QA/UploadPhotoAnswer.jsx
+++ client/src/components/QA/UploadPhotoAnswer.jsx
@@ -3,16 +3,9 @@ import React, { useState } from 'react';
 const UploadPhotoAnswer = (props) => {
   const [image, setImage] = useState([]);
   const [imageCount, setImageCount] = useState(0);
-  const [isModalDisplayed, setModalDisplay] = useState(false);
-  const [source, setSource] = useState('');
   const handleFileUpload = (e) => {
-    console.log(e);
     setImageCount(imageCount + e.target.files.length);
     if (e.target.files.length) {
-      // e.target.files.forEach((file, idx) => { console.log(file) });
-      // console.log(Object.values(e.target.files));
-      // const alias = Object.values(e.target.files);
-      // alias.forEach((file, idx) => { console.log(file.lastModified) });
       const newFiles = image.slice().concat(Object.values(e.target.files));
       setImage(newFiles);
     }
@@ -21,19 +14,6 @@ const UploadPhotoAnswer = (props) => {
     e.preventDefault();
     setImage([]);
     setImageCount(0);
-  };
-  const handleModal = (e) => {
-    console.log('e.target.value', e.target.value);
-    // console.log('URL.createObjectURL(e.target.src)', URL.createObjectURL(e.target.src));
-    // console.log('image', image)
-    // console.log('image', image)
-    const alias = Object.values(e.target.files);
-    console.log(alias)
-    alias.forEach((file, idx) => { console.log(file.lastModified) });
-    setSource(e.target.src);
-  };
-  const toggleModal = () => {
-    setModalDisplay(!isModalDisplayed);
   };
 
   return (
@@ -46,19 +26,12 @@ const UploadPhotoAnswer = (props) => {
         <div className="photo-upload">
           {image.map((img) => (
             <>
-              {/* {console.log(img)}
-              {console.log(URL.createObjectURL(img))} */}
-              <img src={URL.createObjectURL(img)} value={URL.createObjectURL(img)} height="100" alt="" onClick={(e) => handleModal(e)} />
+              <img src={URL.createObjectURL(img)} value={URL.createObjectURL(img)} height="100" alt="" />
             </>
           ))}
           <button className="cancel-upload" type="button" onClick={(e) => handleCancelUpload(e)}>Clear Photos</button>
         </div>
-        {isModalDisplayed && (
-          <div className="overlay">
-            <img src={URL.createObjectURL(source)} alt="product-review" className="review-photo-modal" />
-            {/* <button className="review-form-close-btn" type="button" onClick={togglePhotoModal}>x</button> */}
-          </div>
-        )}
+
       </div>
     </div>
   );
